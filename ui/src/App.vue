0e959e45373a44e0f0ab2bb9805ab4628656ed61
Viktor Nareiko <vnareiko.lt@gmail.com>
2022-12-31 12:12:07 +0200

    Set up base for vue app?

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,24 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoginPage></LoginPage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './pages/auth/LoginPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    LoginPage,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ ui/src/pages/auth/LoginPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center justify-center p-6 min-h-screen bg-indigo-800">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+}
+</script>
