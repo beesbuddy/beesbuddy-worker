ba7d2c66fbf1d40ef3c9434d8fa7257433132269
viktornar <vnareiko.lt@gmail.com>
2023-02-06 22:52:00 +0200

    Cleanup. Refactoring

--- internal/model/subscriber.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "time"
-
-type Subscriber struct {
-	ApiaryId  string    `json:"apiaryId" required:"true"`
-	HiveId    string    `json:"hiveId" required:"true"`
-	CreatedAt time.Time `json:"createdAt" required:"true"`
-}
--- internal/worker/metrics.go
+++ internal/worker/metrics.go
@@ -1,4 +1,4 @@
-package model
+package worker
 
 type Metrics struct {
 	ClientId    string `json:"clientId"`
